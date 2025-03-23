3c8df6956def663a4c7144c7cc562870771f1aca
GE <gevangelinos@pm.me>
2025-03-24 01:05:39 +0200

    Updated log_injector, I tried to declutter it by spreading the its jobs in different files, stil INCOMPLETE

--- PhaseB/.playground/log_injector.py
+++ PhaseB/.playground/log_injector.py
@@ -1,8 +1,8 @@
 import sys
 import os
-import enum
-from typing import NamedTuple, TextIO
 import re
+from typing import NamedTuple, TextIO
+from parse_context import ParseContext, ParserState
 
 
 class StartupArguments(NamedTuple):
@@ -12,40 +12,10 @@ class StartupArguments(NamedTuple):
         hook_name: str
 
 
-class ParserState(enum.Enum):
-        # Valid FSM states:
-        BEFORE_GRAMMAR_SECTION = enum.auto()
-        INSIDE_GRAMMAR_SECTION = enum.auto()
-        AFTER_GRAMMAR_SECTION = enum.auto()
-        PARSING_RULE_BODY = enum.auto()
-        FINISHED_LOG_INSERTION = enum.auto()
-
-        # Invalid FSM states:
-        ERROR_GRAMMAR_DELIMITER_MISSING = enum.auto()
-        ERROR_GRAMMAR_DELIMITER_NOT_STANDALONE = enum.auto()
-
-
-class ParseContext:
-        def __init__(self, logging_function_name: str, hook_name: str) -> None:
-                self._logging_function_name = logging_function_name  # private
-                self._hook_name = hook_name  # private
-                self.brace_depth: int = 0  # We just started reading .y file
-                self.parsed_lines: list[str] = []
-                self.current_state = ParserState.BEFORE_GRAMMAR_SECTION  # We just started, we are certainly outside
-
-        @property
-        def logging_function_name(self) -> str:
-                return self._logging_function_name
-
-        @property
-        def hook_name(self) -> str:
-                return self._hook_name
-
-
 def are_valid_startup_arguments() -> bool:
         if len(sys.argv) < 5:
-                print(f"Usage of {os.path.basename(__file__)}: <input_file> <output_file> <log_function_name> <hook_name>",
-                      file=sys.stderr)
+                print(f"Usage of {os.path.basename(__file__)}: "
+                      f"<input_file> <output_file> <log_function_name> <hook_name>", file=sys.stderr)
                 return False
         if not os.path.isfile(sys.argv[1]):
                 print(f"Input file {sys.argv[1]} is not a file", file=sys.stderr)
@@ -70,33 +40,40 @@ def load_startup_arguments() -> StartupArguments:
 
 def execute_state_before_grammar_section(ctx: ParseContext, fin: TextIO) -> ParserState:
         define_pattern = re.compile(rf"^\s*#define\s+{re.escape(ctx.hook_name)}\b")
-        marker_before_grammar_rules = re.compile(r"^\s*%%")
 
         for line in fin:
+                ctx.line_count += 1
                 if define_pattern.match(line):  # Exclude the definition of the hook
                         continue
                 if "%%" in line and line.strip() != "%%":
-                        return ParserState.ERROR_GRAMMAR_DELIMITER_NOT_STANDALONE
+                        raise ValueError(f"Line {ctx.line_count}: Delimiter %% separating prologue and grammar rules"
+                                         f" is not in a standalone line, please fix")
                 ctx.parsed_lines.append(line)
-                if marker_before_grammar_rules.match(line):
-                        break
+                break
         else:
-                return ParserState.ERROR_GRAMMAR_DELIMITER_MISSING
+                raise ValueError(f"Line {ctx.line_count}: Delimiter %% before grammar rules is missing")
         return ParserState.INSIDE_GRAMMAR_SECTION
 
 
 def execute_state_inside_grammar_section(ctx: ParseContext, fin: TextIO) -> ParserState:
-        header_rule_pattern = re.compile(r'^(\s*)([a-zA-Z_][a-zA-Z0-9_]*)(\s*):')
-
-        current_rule_header: str = ""
+        char_stream: list[str] = []
         for line in fin:
-                header_match = header_rule_pattern.match(line)
-                if header_match:
-                        current_rule_header = header_match.group(2) # Second group, enclosed in () contains the rule name.
+                if "%%" in line and line.strip() != "%%":
+                        raise ValueError(f"Line {ctx.line_count}: Delimiter %% separating epilogue and grammar rules"
+                                         f" is not in a standalone line, please fix")
+                if "%%" in line:
+                        break
+                char_stream.extend(line)
+        else:
+                raise ValueError(f"Line {ctx.line_count}: Delimiter %% after grammar rules is missing, please fix")
 
+        # If control reaches here, we exited for-loop with break.
+        # We need to run the FSMs for parsing the grammar rules.
+        # Basically we need to call the manager for grammar section.
 
-
-
+        # When control reaches here, it means we have parsed all the grammar rules.
+        ctx.parsed_lines.append(line) # TODO: We append this line after we append the INJECTED GRAMMAR RULES
+        return ParserState.AFTER_GRAMMAR_SECTION
 
 
 def parser_fsm(ctx: ParseContext, fin: TextIO) -> None:
@@ -108,21 +85,11 @@ def parser_fsm(ctx: ParseContext, fin: TextIO) -> None:
                                 return
                         case ParserState.AFTER_GRAMMAR_SECTION:
                                 return
-                        case ParserState.PARSING_RULE_BODY:
-                                return
                         case ParserState.FINISHED_LOG_INSERTION:
                                 print("Log insertion completed")
                                 return
-                        case ParserState.ERROR_GRAMMAR_DELIMITER_NOT_STANDALONE:
-                                print("Delimiter %% separating grammar rules is not in a standalone line, please fix",
-                                      file=sys.stderr)
-                                return
-                        case ParserState.ERROR_GRAMMAR_DELIMITER_MISSING:
-                                print("Delimiter %% separating grammar rules is not found", file=sys.stderr)
-                                return
                         case _:
-                                print("Parsing FSM entered unknown state", file=sys.stderr)
-                                sys.exit(2)
+                                raise AssertionError(f"parser_fsm() enter unknown state, this should never happen")
 
 
 def parser_manager(startup_arguments: StartupArguments) -> None:
