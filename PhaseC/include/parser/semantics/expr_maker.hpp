0f55ff892073385e3416001a0a3873cdb4315f7d
GE <gevangelinos@pm.me>
2025-05-28 03:07:24 +0300

    new exprmaker interface

--- PhaseC/include/parser/semantics/expr_maker.hpp
+++ PhaseC/include/parser/semantics/expr_maker.hpp
@@ -1,13 +1,12 @@
 // TODO: make const_bool_true, const_bool_false, const_int = 1 static expressions and reusable.
-// Also you should you should be able to modify their location, so QuadHandler can take their latest
-// location of use, and append it to the quad it is making.
+// Also you should you should be able to modify their loc, so QuadHandler can take their latest
+// loc of use, and append it to the quad it is making.
 
 #ifndef ALPHA_EXPR_MAKER_HPP
 #define ALPHA_EXPR_MAKER_HPP
 
 #include "expr_validator.hpp"
 #include "core/alpha_basics.hpp"
-#include "core/alpha_ir.hpp"
 #include "core/alpha_location.hpp"
 #include "parser/alpha_parser_context.hpp"
 #include "parser/alpha_semantic_driver.hpp"
@@ -20,21 +19,25 @@ public:
     explicit ExprMaker(SemanticDriver *sd);
     ~ExprMaker() noexcept;
 
-    [[nodiscard]] Expr *make_arithmetic_expr(IOPCode iopc, const Expr *left, const Expr *right,
-                                             SourceLocation left_loc, SourceLocation right_loc,
-                                             SourceLocation expr_loc);
-    [[nodiscard]] Expr *make_assign_expr(const Expr *rvalue, SourceLocation expr_loc);
-    [[nodiscard]] Expr *make_bool_expr(SourceLocation expr_loc);
-    [[nodiscard]] Expr *make_const_bool_expr(bool bool_value, SourceLocation expr_loc);
-    [[nodiscard]] Expr *make_const_int_expr(i64 int_value, SourceLocation expr_loc);
-    [[nodiscard]] Expr *make_const_float_expr(f64 float_value, SourceLocation expr_loc);
-    [[nodiscard]] Expr *make_const_string_expr(const char *str_value, SourceLocation expr_loc);
-    [[nodiscard]] Expr *make_const_nil_expr(SourceLocation expr_loc);
-    [[nodiscard]] Expr *make_new_table_expr(SourceLocation expr_loc);
-    [[nodiscard]] Expr *make_program_function_expr(const Function *function_symbol);
-    [[nodiscard]] Expr *make_table_item_expr(const Expr *lvalue, const Expr *index,
-                                             SourceLocation expr_loc);
-    [[nodiscard]] Expr *make_expr_variable(const Symbol *symbol, SourceLocation expr_loc);
+    /// @Note: All expr_loc(s) are the loc of the resulting expression.
+    [[nodiscard]] const ArithmeticExpr *make_arithmetic_expr(SourceLocation expr_loc);
+    [[nodiscard]] const AssignExpr *make_assign_expr(const Expr *rvalue, SourceLocation expr_loc);
+    [[nodiscard]] BoolExpr *make_bool_expr(SourceLocation expr_loc);
+    [[nodiscard]] const ConstBoolExpr *make_const_bool_expr(
+        bool bool_value, SourceLocation expr_loc);
+    [[nodiscard]] const ConstIntExpr *make_const_int_expr(i64 int_value, SourceLocation expr_loc);
+    [[nodiscard]] const ConstFloatExpr *make_const_float_expr(
+        f64 float_value, SourceLocation expr_loc);
+    [[nodiscard]] const ConstStringExpr *make_const_string_expr(
+        const char *str_value, SourceLocation expr_loc);
+    [[nodiscard]] const ConstNilExpr *make_nil_expr(SourceLocation expr_loc);
+    [[nodiscard]] const NewTableExpr *make_new_table_expr(SourceLocation expr_loc);
+    [[nodiscard]] const ProgFuncExpr *make_program_function_expr(
+        SourceLocation expr_loc, const Function *function_symbol);
+    [[nodiscard]] const TableItemExpr *make_table_item_expr(const Expr *lvalue, const Expr *index,
+                                                            SourceLocation expr_loc);
+    [[nodiscard]] const VariableExpr *make_variable_expr(const Symbol *symbol,
+                                                         SourceLocation expr_loc);
 
 private:
     SemanticDriver *const sd_;
@@ -49,193 +52,145 @@ ExprMaker::ExprMaker(SemanticDriver *const sd)
 
 inline ExprMaker::~ExprMaker() noexcept
 {
-    for(const Expr *e : expr_sink_)
+    for (const Expr *e : expr_sink_)
     {
         DEBUG_SMART_ASSERT(!!e);
-        if(e->type == Expr::Type::CONST_STRING)
-            delete[] e->const_str;
-        delete e;
+        switch (e->type)
+        {
+            // clang-format off
+        case Expr::Type::ARITHMETIC_EXPR: delete static_cast<const ArithmeticExpr *>(e); break;
+        case Expr::Type::ASSIGN_EXPR: delete static_cast<const AssignExpr *>(e);         break;
+        case Expr::Type::BOOL_EXPR: delete static_cast<const BoolExpr *>(e);             break;
+        case Expr::Type::CONST_BOOL: delete static_cast<const ConstBoolExpr *>(e);       break;
+        case Expr::Type::CONST_INT: delete static_cast<const ConstIntExpr *>(e);         break;
+        case Expr::Type::CONST_FLOAT: delete static_cast<const ConstFloatExpr *>(e);     break;
+        case Expr::Type::CONST_STRING: delete static_cast<const ConstStringExpr *>(e);   break;
+        case Expr::Type::CONST_NIL: delete static_cast<const ConstNilExpr *>(e);         break;
+        case Expr::Type::LIBRARY_FUNCTION: delete static_cast<const LibFuncExpr *>(e);   break;
+        case Expr::Type::PROGRAM_FUNCTION: delete static_cast<const ProgFuncExpr *>(e);  break;
+        case Expr::Type::NEW_TABLE: delete static_cast<const NewTableExpr *>(e);         break;
+        case Expr::Type::TABLE_ITEM: delete static_cast<const TableItemExpr *>(e);       break;
+        case Expr::Type::VARIABLE: delete static_cast<const VariableExpr *>(e);          break;
+        // clang-format on
+            [[unlikely]] default: UNREACHABLE(FMT::format(
+                "Unknown Expr::Type. Expr::Type's int value = {}", static_cast<int>(e->type)));
+        }
     }
 }
 
-inline Expr *
-ExprMaker::make_arithmetic_expr(
-    const IOPCode iopc,
-    const Expr *const left,
-    const Expr *const right,
-    const SourceLocation left_loc,
-    const SourceLocation right_loc,
-    const SourceLocation expr_loc)
+inline const ArithmeticExpr *
+ExprMaker::make_arithmetic_expr(const SourceLocation expr_loc)
 {
-    DEBUG_SMART_ASSERT(!!left, !!right);
-    sd_->expr_validator_.report_if_not_arithmetic(iopc, left, left_loc, OperandSide::LEFT);
-    sd_->expr_validator_.report_if_not_arithmetic(iopc, right, right_loc, OperandSide::RIGHT);
-
-    if (sd_->sem_opts_.fold_arithmetic)
-        if (SemUtils::is_const_numeric_expr(left) && SemUtils::is_const_numeric_expr(right))
-            if (Expr *folded = sd_->expr_folder_.fold_arithmetic(iopc, left, right, expr_loc))
-                return folded;
-
-    Expr *arithmetic_expr = new Expr{
-        .type = Expr::Type::ARITHMETIC_EXPR,
-        .symbol = parse_ctx_->new_temp(),
-        .location = expr_loc,
-        .union_control = nullptr,
-    };
-
+    const auto arithmetic_expr = new const ArithmeticExpr(
+        expr_loc, parse_ctx_->new_temp());
     expr_sink_.push_back(arithmetic_expr);
-    sd_->quad_handler.emit_next_quad(iopc, left, right, arithmetic_expr, expr_loc);
     return arithmetic_expr;
 }
 
-inline Expr *
+inline const AssignExpr *
 ExprMaker::make_assign_expr(const Expr *const rvalue, const SourceLocation expr_loc)
 {
     DEBUG_SMART_ASSERT(!!rvalue);
-    Expr *assign_expr = new Expr{
-        .type = Expr::Type::ASSIGN_EXPR,
-        .symbol = rvalue->symbol,
-        .location = expr_loc,
-        .index = rvalue->index,
-    };
+    const auto assign_expr = new const AssignExpr(expr_loc, rvalue);
     expr_sink_.push_back(assign_expr);
     return assign_expr;
 }
 
-inline Expr *ExprMaker::make_bool_expr(const SourceLocation expr_loc)
+inline BoolExpr *ExprMaker::make_bool_expr(const SourceLocation expr_loc)
 {
-    Expr *bool_expr = new Expr{
-        .type = Expr::Type::BOOLEAN_EXPR,
-        .symbol = parse_ctx_->new_temp(),
-        .location = expr_loc,
-        .union_control = nullptr,
-        .backpatch_lists = new BackpatchLists{},
-    };
+    const auto bool_expr = new BoolExpr(expr_loc, parse_ctx_->new_temp());
     expr_sink_.push_back(bool_expr);
     return bool_expr;
 }
 
-inline Expr *
+inline const ConstBoolExpr *
 ExprMaker::make_const_bool_expr(const bool bool_value, const SourceLocation expr_loc)
 {
-    Expr *bool_expr = new Expr{
-        .type = Expr::Type::CONST_BOOL,
-        .symbol = nullptr,
-        .location = expr_loc,
-        .const_bool = bool_value,
-    };
-    expr_sink_.push_back(bool_expr);
-    return bool_expr;
+    const auto const_bool_expr = new const ConstBoolExpr(expr_loc, bool_value);
+    expr_sink_.push_back(const_bool_expr);
+    return const_bool_expr;
 }
 
-inline Expr *
+inline const ConstIntExpr *
 ExprMaker::make_const_int_expr(const i64 int_value, const SourceLocation expr_loc)
 {
-    Expr *int_expr = new Expr{
-        .type = Expr::Type::CONST_INT,
-        .symbol = nullptr,
-        .location = expr_loc,
-        .const_int = int_value,
-    };
-    expr_sink_.push_back(int_expr);
-    return int_expr;
+    const auto const_int_expr = new const ConstIntExpr(expr_loc, int_value);
+    expr_sink_.push_back(const_int_expr);
+    return const_int_expr;
 }
 
-inline Expr *
+inline const ConstFloatExpr *
 ExprMaker::make_const_float_expr(const f64 float_value, const SourceLocation expr_loc)
 {
-    Expr *float_expr = new Expr{
-        .type = Expr::Type::CONST_FLOAT,
-        .symbol = nullptr,
-        .location = expr_loc,
-        .const_real = float_value,
-    };
-    expr_sink_.push_back(float_expr);
-    return float_expr;
+    const auto const_float_expr = new const ConstFloatExpr(expr_loc, float_value);
+    expr_sink_.push_back(const_float_expr);
+    return const_float_expr;
 }
 
-inline Expr *
+inline const ConstStringExpr *
 ExprMaker::make_const_string_expr(const char *const str_value, const SourceLocation expr_loc)
 {
     DEBUG_SMART_ASSERT(!!str_value);
-    Expr *str_expr = new Expr{
-        .type = Expr::Type::CONST_STRING,
-        .symbol = nullptr,
-        .location = expr_loc,
-        .const_str = Utils::cstrdup(str_value),
-    };
-    expr_sink_.push_back(str_expr);
-    return str_expr;
+    const auto const_str_expr = new const ConstStringExpr(expr_loc, str_value);
+    expr_sink_.push_back(const_str_expr);
+    return const_str_expr;
 }
 
-inline Expr *
-ExprMaker::make_const_nil_expr(const SourceLocation expr_loc)
+inline const ConstNilExpr *
+ExprMaker::make_nil_expr(const SourceLocation expr_loc)
 {
-    Expr *nil_expr = new Expr{
-        .type = Expr::Type::CONST_NIL,
-        .symbol = nullptr,
-        .location = expr_loc,
-        .union_control = nullptr,
-    };
+    const auto nil_expr = new const ConstNilExpr(expr_loc);
     expr_sink_.push_back(nil_expr);
     return nil_expr;
 }
 
-inline Expr *ExprMaker::make_new_table_expr(const SourceLocation expr_loc)
+inline const NewTableExpr *ExprMaker::make_new_table_expr(const SourceLocation expr_loc)
 {
-    Expr *new_table_expr = new Expr{
-        .type = Expr::Type::NEW_TABLE,
-        .symbol = parse_ctx_->new_temp(),
-        .location = expr_loc,
-        .union_control = nullptr,
-    };
+    const auto new_table_expr = new const NewTableExpr(expr_loc, parse_ctx_->new_temp());
     expr_sink_.push_back(new_table_expr);
     return new_table_expr;
 }
 
-inline Expr *
-ExprMaker::make_program_function_expr(const Function *const function_symbol)
+inline const ProgFuncExpr *
+ExprMaker::make_program_function_expr(
+    const SourceLocation expr_loc,
+    const Function *const function_symbol)
 {
     DEBUG_SMART_ASSERT(!!function_symbol);
-    Expr *progfunc_expr = new Expr{
-        .type = Expr::Type::PROGRAM_FUNCTION,
-        .symbol = function_symbol,
-        .location = function_symbol->location,
-        .union_control = nullptr,
-    };
+    const auto progfunc_expr = new const ProgFuncExpr(expr_loc, function_symbol);
     expr_sink_.push_back(progfunc_expr);
     return progfunc_expr;
 }
 
-inline Expr *
+inline const TableItemExpr *
 ExprMaker::make_table_item_expr(
     const Expr *const lvalue,
     const Expr *const index,
     const SourceLocation expr_loc)
 {
-    DEBUG_SMART_ASSERT(!!lvalue, !!index);
-    Expr *table_item_expr = new Expr{
-        .type = Expr::Type::TABLE_ITEM,
-        .symbol = lvalue->symbol,
-        .location = expr_loc,
-        .index = index,
-    };
+    DEBUG_SMART_ASSERT(
+        !!lvalue,
+        !!index,
+        !SemUtils::is_rvalue_expr(lvalue),
+        SemUtils::is_expr_with_symbol(lvalue)
+    );
+    const auto expr_w_symbol = static_cast<const ExprWSymbol *>(REQUIRE_PTR(lvalue));
+    const auto lvalue_symbol = static_cast<const Symbol *>(expr_w_symbol->symbol);
+    const auto table_item_expr = new const TableItemExpr(
+        expr_loc,
+        lvalue_symbol,
+        index);
     expr_sink_.push_back(table_item_expr);
     return table_item_expr;
 }
 
-inline Expr *
+inline const VariableExpr *
 ExprMaker::make_variable_expr(const Symbol *const symbol, const SourceLocation expr_loc)
 {
     DEBUG_SMART_ASSERT(!!symbol);
-    Expr *expr_lvalue = new Expr{
-        .type = Expr::Type::VARIABLE,
-        .symbol = symbol,
-        .location = expr_loc,
-        .union_control = nullptr,
-    };
-    expr_sink_.push_back(expr_lvalue);
-    return expr_lvalue;
+    const auto variable_expr = new const VariableExpr(expr_loc, symbol);
+    expr_sink_.push_back(variable_expr);
+    return variable_expr;
 }
 } // namespace Alpha
 #endif //ALPHA_EXPR_MAKER_HPP
